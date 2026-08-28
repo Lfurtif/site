40b7e3d33ddf5249f3d422fe8333974405ded242
Lfurtif <leojerogasp2@gmail.com>
2026-08-28 15:40:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(`https://api.github.com/users/${GITHUB_USERNAME}/repos?sort=updated&per_page=6`)
         .then(res => res.json())
         .then(repos => {
-            const container = document.getElementById('repo-list');
-            container.innerHTML = repos.map(repo => `
+            document.getElementById('repo-list').innerHTML = repos.map(repo => `
                 <a href="${repo.html_url}" target="_blank" class="repo-item">
                     <h4>${repo.name}</h4>
                     <p>${repo.description ? repo.description.substring(0, 60) + '...' : 'No description.'}</p>
